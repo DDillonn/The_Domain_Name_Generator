944c9581bdf1866396c44991466aa158706b5264
DDillonn <Saltnessdillon@gmail.com>
2022-04-01 01:34:48 +0000

    Created Repo

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let domainName = [];
+  let pronoun = ["the", "you"];
+  let adj = ["elegant", "obtuse"];
+  let noun = ["servant", "subordinate"];
+  let domain = [".com", ".io"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < pronoun.length; j++) {
+      for (let k = 0; k < pronoun.length; k++) {
+        for (let l = 0; l < pronoun.length; l++) {
+          domainName.push(" " + pronoun[i] + adj[j] + noun[k] + domain[l]);
+        }
+      }
+    }
+  }
+  console.log(domainName);
+  document.getElementById("domain.results").innerHTML = domainName;
 };
